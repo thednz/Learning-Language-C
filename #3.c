08a63e650a050cc9bab68d8f8c1083b54e8d93d2
augustindnz <augustinpro90@gmail.com>
2024-12-08 20:46:27 -0300

    progress slow

--- #3.c
+++ #3.c
@@ -7,7 +7,7 @@ int main()
     char respost[4], name[20];
     printf("hello my friend,what your name?");
     scanf("%s", name);
-    printf("\ni'm need you %s for written code, of corse? (yes or no)", name);
+    printf("\ni'm need you %s for written code, of corse? (yes or no) ", name);
     scanf("%3s", respost);
     printf("\nnow talk me a number whole (a):");
     scanf("%d", &a);
@@ -17,25 +17,9 @@ int main()
     a = a + b;
     b = b - a;
     media = (a + b) / 2;
-    if (a <= 10)
-    {
-        if (b <= 13)
-        {
-            printf("\noh shit");
-        }
-        else
-        {
-            printf("\noh ");
-        }
-        printf("\nanything but not%s ", name);
-    }
-    else
-    {
-        printf("\ndon't make love in SP pls!\n ");
-    }
-    system("cls");
+    
     printf("\nis here your result's %s, media %d of item (a) %d and result item (b) %d\n", name, media, a, b);
-
+    printf("finish aplication");
     system("pause");
     return 0;
 }
--- #4.c
+++ #4.c
@@ -3,9 +3,15 @@
 
 int main()
 {
-    printf("structure loop");
+    float numberTest01, numbeTest02, soma;
+    printf("here teste with table of values in c!! \n");
+    printf("choose an number! whole \n");
+    scanf("%f", &numberTest01);
+    printf("now other whole \n");
+    scanf("%f", &numbeTest02);
+    soma = numberTest01 + numbeTest02;
 
-
-
+    printf("Result of numbers is %0.3f and %0.3f and finale %0.3f", numberTest01, numbeTest02, soma);
+    system("pause");
     return 0;
 }
