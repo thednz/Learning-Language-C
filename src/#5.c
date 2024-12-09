5d6652284dfb65bd901cdbc5d43448dbcd55cf2f
augustindnz <augustinpro90@gmail.com>
2024-12-08 22:59:48 -0300

    last of the nite

--- src/#5.c
+++ src/#5.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
+/* copy input to output; 1st version */
+int main()
+{
+    int i = 0;
+    for (i = 0; i <= 50; i++)
+    {
+        printf("hi,are you?\n ");
+    }
+    return 0;
+}
