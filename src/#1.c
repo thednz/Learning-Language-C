00294484ac766697ee47b4280dfa0ee289c31dc8
augustindnz <augustinpro90@gmail.com>
2024-12-10 21:47:43 -0300

    started

--- src/#1.c
+++ src/#1.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-    char nome[12];           // array = string although with selection numbers.
-    int numberInt = 0;       // int number normally.
-    short numberShort = 0;   // minimum size of short and int in C.
-    long numberLong = 0;     // minimum size of long in C, actual size of int for most modern C compilers!
-    float numberFloat = 0;   // float number is equal int although with decimal!
-    double numberDouble = 0; // double number have decimal of method more  eficient!
 
-    printf("Just testing words being written to the console\n ");
-
-    return (0);
+    return 0;
 }
